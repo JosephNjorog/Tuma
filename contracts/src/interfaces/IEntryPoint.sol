4ce47127eeae11870ea85fa37038242677990852
JosephNjorog <njorojoe11173@gmail.com>
2026-06-16 21:51:54 +0300

    fix(contracts): move IAggregator out of nested interface scope
    Solidity doesn't allow interfaces nested inside other interfaces;
    IAggregator must be declared at file scope to compile.

--- contracts/src/interfaces/IEntryPoint.sol
+++ contracts/src/interfaces/IEntryPoint.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.24;
 
 import "./UserOperation.sol";
 
+interface IAggregator {
+    function validateSignatures(UserOperation[] calldata userOps, bytes calldata signature) external view;
+    function validateUserOpSignature(UserOperation calldata userOp) external view returns (bytes memory sigForUserOp);
+    function aggregateSignatures(UserOperation[] calldata userOps) external view returns (bytes memory aggregatedSignature);
+}
+
 interface IEntryPoint {
     /**
      * Execute a batch of UserOperations.
@@ -62,11 +68,5 @@ interface IEntryPoint {
         UserOperation[] userOps;
         IAggregator aggregator;
         bytes signature;
-    }
-
-    interface IAggregator {
-        function validateSignatures(UserOperation[] calldata userOps, bytes calldata signature) external view;
-        function validateUserOpSignature(UserOperation calldata userOp) external view returns (bytes memory sigForUserOp);
-        function aggregateSignatures(UserOperation[] calldata userOps) external view returns (bytes memory aggregatedSignature);
     }
 }
